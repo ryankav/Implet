d9ba2a61b23e87f9174f06b16260b845cab109cf
Ryan <ryan.p.kav@gmail.com>
2021-12-20 13:01:09 +0000

    Storybook now builds with a material ui theme provider add on

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { red } from '@mui/material/colors';
+
+// A custom theme for this app
+const theme = {
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+};
+
+export default theme;
